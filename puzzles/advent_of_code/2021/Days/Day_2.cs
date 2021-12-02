ccfe511d3f27d1585bb2e8b18b378a7b09d8ade8
LEPT0N <dan.mitchell@outlook.com>
2021-12-01 23:52:00 -0800

    Advent of Code 2021 Day 2

---
+++ puzzles/advent_of_code/2021/Days/Day_2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_2021.Days
+{
+    internal class Day_2
+    {
+        public static void Part_1(string input)
+        {
+            string[] lines = System.IO.File.ReadAllLines(input);
+
+            int horizontal_position = 0;
+            int depth = 0;
+
+            foreach (string line in lines)
+            {
+                string[] split_line = line.Split(' ');
+                string direction = split_line[0];
+                int amount = int.Parse(split_line[1]);
+
+                switch (split_line[0])
+                {
+                    case "forward": horizontal_position += amount; break;
+                    case "down": depth += amount; break;
+                    case "up": depth -= amount; break;
+                }
+            }
+
+            Console.WriteLine("Total forward position = " + horizontal_position);
+            Console.WriteLine("Total depth = " + depth);
+            Console.WriteLine("Result = " + (depth * horizontal_position));
+        }
+        public static void Part_2(string input)
+        {
+            string[] lines = System.IO.File.ReadAllLines(input);
+
+            int horizontal_position = 0;
+            int depth = 0;
+            int aim = 0;
+
+            foreach (string line in lines)
+            {
+                string[] split_line = line.Split(' ');
+                string direction = split_line[0];
+                int amount = int.Parse(split_line[1]);
+
+                switch (split_line[0])
+                {
+                    case "forward":
+                        horizontal_position += amount;
+                        depth += aim * amount;
+                        break;
+
+                    case "down": aim += amount; break;
+                    case "up": aim -= amount; break;
+                }
+            }
+
+            Console.WriteLine("Total forward position = " + horizontal_position);
+            Console.WriteLine("Total depth = " + depth);
+            Console.WriteLine("Total aim = " + aim);
+            Console.WriteLine("Result = " + (depth * horizontal_position));
+        }
+    }
+}
--- puzzles/advent_of_code/2021/Program.cs
+++ puzzles/advent_of_code/2021/Program.cs
@@ -17,6 +17,8 @@ namespace advent_of_code_2021
             {
                 case (1, 1): Day_1.Part_1(input); break;
                 case (1, 2): Day_1.Part_2(input); break;
+                case (2, 1): Day_2.Part_1(input); break;
+                case (2, 2): Day_2.Part_2(input); break;
 
                 default:
                     Console.WriteLine("Unexpected day " + args[0]);
