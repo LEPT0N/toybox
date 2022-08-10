d6f356ad1e25b31d510954958d7023f175f4ada0
LEPT0N <dan.mitchell@outlook.com>
2022-08-09 23:38:00 -0700

    Advent of Code 2019 project

---
+++ puzzles/advent_of_code/2019/advent_of_code_2019.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using advent_of_code_2019.days;
+
+namespace advent_of_code_2019
+{
+    internal class advent_of_code_2019
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int day = Int32.Parse(args[0]);
+            int part = Int32.Parse(args[1]);
+            string input = args[2];
+
+            bool pretty = (args.Length >= 4 && args[3] == "pretty");
+
+            switch (day, part)
+            {
+                default:
+                    Console.WriteLine("Unexpected day '{0}'", args[0]);
+                    break;
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Time taken = {0}", stopwatch.Elapsed);
+            Console.ResetColor();
+        }
+    }
+}
