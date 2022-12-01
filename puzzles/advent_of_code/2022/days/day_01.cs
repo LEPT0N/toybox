e1a58e311881ee8aa0e084fab7c7b203d56bd048
LEPT0N <dan.mitchell@outlook.com>
2022-12-01 00:48:07 -0800

    Advent of Code 2022 Day 01

--- puzzles/advent_of_code/2022/days/day_01.cs
+++ puzzles/advent_of_code/2022/days/day_01.cs
@@ -9,32 +9,49 @@ namespace advent_of_code_2022.days
 {
     internal class day_01
     {
-        [DebuggerDisplay("todo", Type = "c_type")]
-        internal class c_type
-        {
-        }
-
-        internal static void parse_input(
+        internal static int[][] parse_input(
             in string input,
             in bool pretty)
         {
             c_input_reader input_reader = new c_input_reader(input);
 
+            List<int[]> all_groups = new List<int[]>();
+            List<int> current_group = new List<int>();
+
             while (input_reader.has_more_lines())
             {
-                input_reader.read_line();
+                string line = input_reader.read_line();
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    all_groups.Add(current_group.ToArray());
+                    current_group = new List<int>();
+                }
+                else
+                {
+                    current_group.Add(int.Parse(line));
+                }
             }
+
+            if (current_group.Any())
+            {
+                all_groups.Add(current_group.ToArray());
+            }
+
+            return all_groups.ToArray();
         }
 
         public static void part_1(
             string input,
             bool pretty)
         {
-            parse_input(input, pretty);
+            int[][] groups = parse_input(input, pretty);
+
+            int max_group_sum = groups.Max(group => group.Sum());
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine();
-            Console.WriteLine("Result = {0}", 0);
+            Console.WriteLine("Result = {0}", max_group_sum);
             Console.ResetColor();
         }
 
@@ -42,11 +59,15 @@ namespace advent_of_code_2022.days
             string input,
             bool pretty)
         {
-            // parse_input(input, pretty);
+            int[][] groups = parse_input(input, pretty);
+
+            int[] max_groups = groups.Select(group => group.Sum()).OrderByDescending(x => x).Take(3).ToArray();
+
+            int max_groups_sum = max_groups.Sum();
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine();
-            Console.WriteLine("Result = {0}", 0);
+            Console.WriteLine("Result = {0}", max_groups_sum);
             Console.ResetColor();
         }
     }
