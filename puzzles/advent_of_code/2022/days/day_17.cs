e8954514be665c39aa80357dc48b3cdf7285b933
LEPT0N <dan.mitchell@outlook.com>
2022-12-17 23:43:21 -0800

    Advent of Code 2022 Day 17 Part 2

--- puzzles/advent_of_code/2022/days/day_17.cs
+++ puzzles/advent_of_code/2022/days/day_17.cs
@@ -121,6 +121,13 @@ namespace advent_of_code_2022.days
                 return (UInt64)highest_nonempty_spot + rows_deleted;
             }
 
+            public void set_highest_nonempty_spot(UInt64 total_highest_nonempty_spot)
+            {
+                // Pretend to move the tower up/down by setting how many rows have been deleted
+                // so that get_highest_nonempty_spot will match the input to this method.
+                rows_deleted = total_highest_nonempty_spot - (UInt64)highest_nonempty_spot; 
+            }
+
             internal void set_piece(c_piece piece, e_spot_type type)
             {
                 foreach (c_vector spot in piece.positions)
@@ -150,6 +157,106 @@ namespace advent_of_code_2022.days
                         display();
                     }
                 } while (current_piece != null);
+            }
+
+            private class c_board_thumbprint
+            {
+                // Reference data
+                public UInt64 pieces_settled_count { get; set; }
+                public UInt64 highest_nonempty_spot { get; set; }
+
+                // Equivalence data
+                // The current wind, current piece, and shape of the top of the tower is probably enough to detect equivalent tower states.
+                // An exact equivalence would replace highest_nonempty_spots with a more exact definition of the shape of the top of the tower.
+                // But this worked.
+                public int current_spawn_piece { get; set; }
+                public int current_wind_direction { get; set; }
+
+                public int[] highest_nonempty_spots { get; set; }
+            }
+
+            private class c_board_thumbprint_comparer : IEqualityComparer<c_board_thumbprint>
+            {
+                public bool Equals(c_board_thumbprint a, c_board_thumbprint b)
+                {
+                    if (a.current_spawn_piece != b.current_spawn_piece ||
+                        a.current_wind_direction != b.current_wind_direction)
+                    {
+                        return false;
+                    }
+
+                    for (int i = 0; i < a.highest_nonempty_spots.Length; i++)
+                    {
+                        if (a.highest_nonempty_spots[i] != b.highest_nonempty_spots[i])
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+
+                public int GetHashCode(c_board_thumbprint a)
+                {
+                    // Assume at least three columns or some of the pieces don't fit.
+                    // It's just a hash, collisions are fine.
+                    return HashCode.Combine(
+                        a.current_spawn_piece,
+                        a.current_wind_direction,
+                        a.highest_nonempty_spots[0],
+                        a.highest_nonempty_spots[1],
+                        a.highest_nonempty_spots[2]);
+                }
+            }
+
+            public (UInt64 loop_1_start, UInt64 loop_1_start_height, UInt64 loop_2_start, UInt64 loop_2_start_height) move_until_loop_detected(bool pretty)
+            {
+                // Run the game until a loop is detected (since the placed blocks and wind input is in a set pattern, then if we run the game long enough then
+                // we will eventually loop back to a previous state, just with a higher tower.
+
+                UInt64 pieces_settled_count = 0;
+
+                // Keep track of all states we've seen
+                HashSet<c_board_thumbprint> board_thumbprints = new HashSet<c_board_thumbprint>(new c_board_thumbprint_comparer());
+
+                while (true)
+                {
+                    // Place a piece
+                    move_until_settled(pretty);
+                    pieces_settled_count++;
+
+                    // Create a thumbprint for the current board state.
+                    int[] highest_nonempty_spots_copy = new int[highest_nonempty_spots.Length];
+                    Array.Copy(highest_nonempty_spots, highest_nonempty_spots_copy, highest_nonempty_spots.Length);
+
+                    c_board_thumbprint new_thumbprint = new c_board_thumbprint
+                    {
+                        pieces_settled_count = pieces_settled_count,
+                        highest_nonempty_spot = get_highest_nonempty_spot(),
+
+                        current_spawn_piece = current_spawn_piece,
+                        current_wind_direction = current_wind_direction,
+                        highest_nonempty_spots = highest_nonempty_spots_copy,
+                    };
+
+                    if (board_thumbprints.TryGetValue(new_thumbprint, out c_board_thumbprint existing_thumbprint))
+                    {
+                        // Our new thumbprint is equivalent to an older one! We found a loop, so return.
+
+                        return (
+                            existing_thumbprint.pieces_settled_count,
+                            existing_thumbprint.highest_nonempty_spot,
+                            new_thumbprint.pieces_settled_count,
+                            new_thumbprint.highest_nonempty_spot
+                            );
+                    }
+                    else
+                    {
+                        // Add the current thumbprint to the collection.
+
+                        board_thumbprints.Add(new_thumbprint);
+                    }
+                }
             }
 
             public void move()
@@ -347,9 +454,8 @@ namespace advent_of_code_2022.days
                 .ToArray();
         }
 
-        public static void part_worker(
+        internal static c_board parse_board(
             string input,
-            UInt64 piece_count,
             bool pretty)
         {
             e_direction[] directions = parse_input(input, pretty);
@@ -419,9 +525,18 @@ namespace advent_of_code_2022.days
                 board.display();
             }
 
-            DateTime previous = DateTime.UtcNow;
-            TimeSpan display_period = TimeSpan.FromSeconds(1);
+            return board;
+        }
 
+        public static void part_1(
+            string input,
+            bool pretty)
+        {
+            c_board board = parse_board(input, pretty);
+
+            UInt64 piece_count = 2022;
+            
+            // Place {piece_count} pieces
             for (UInt64 i = 0; i < piece_count; i++)
             {
                 board.move_until_settled(pretty);
@@ -430,36 +545,82 @@ namespace advent_of_code_2022.days
                 {
                     board.display();
                 }
-
-                DateTime now = DateTime.UtcNow;
-                if (now - previous >= display_period)
-                {
-                    previous = now;
-
-                    Console.WriteLine($"Placed {i} / {piece_count}...");
-                }
             }
 
-            board.display();
+            if (!pretty)
+            {
+                board.display();
+            }
 
+            // And display the total tower height.
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine();
             Console.WriteLine("Result = {0}", board.get_highest_nonempty_spot());
             Console.ResetColor();
         }
 
-        public static void part_1(
-            string input,
-            bool pretty)
-        {
-            part_worker(input, 2022, pretty);
-        }
-
         public static void part_2(
             string input,
             bool pretty)
         {
-            part_worker(input, 1000000000000, pretty);
+            // At first I figured I could solve this by chopping off the bottom of the board to wherever the board became inaccessible to newly
+            // spawned pieces, and just keep track of how many rows had been sent to the shadow realm. This seemed to work! however...
+            // By my estimates, using part_1 to place 1000000000000 pieces would take between a week and a month for this program to run.
+            // So instead we need to do something clever.
+            // Because the inputs (wind and pieces) are on set deterministic loops, eventually we will find that the relevant board state loops.
+            // With this in mind, we can solve this problem by:
+            //      1. Run the game up until the first loop.
+            //      2. Quickly compute how many times the game will loop on the way to 1T pieces, and how tall the tower will be at that point.
+            //      3. Continue running the game after the last loop and stop at 1T pieces placed.
+
+            c_board board = parse_board(input, pretty);
+
+            UInt64 total_piece_count = 1000000000000;
+
+            // Run until we detect the first loop.
+            var loop_detection_result = board.move_until_loop_detected(pretty);
+
+            Console.WriteLine("Found loop from {0} (height {1}) to {2} (height {3})",
+                loop_detection_result.loop_1_start,
+                loop_detection_result.loop_1_start_height,
+                loop_detection_result.loop_2_start,
+                loop_detection_result.loop_2_start_height);
+
+            // Figure out how many loops will run, and the state of the board will be after all those loops.
+            UInt64 pieces_placed_before_loops = loop_detection_result.loop_1_start;
+            UInt64 height_before_loops = loop_detection_result.loop_1_start_height;
+
+            UInt64 pieces_placed_per_loop = (loop_detection_result.loop_2_start - loop_detection_result.loop_1_start);
+            UInt64 height_per_loop = (loop_detection_result.loop_2_start_height - loop_detection_result.loop_1_start_height);
+
+            UInt64 loop_count = (total_piece_count - pieces_placed_before_loops) / pieces_placed_per_loop;
+            UInt64 placed_after_loops = pieces_placed_before_loops + (pieces_placed_per_loop * loop_count);
+            UInt64 height_after_loops = height_before_loops + (height_per_loop * loop_count);
+
+            // Push the board up to this new post-loop height
+            board.set_highest_nonempty_spot(height_after_loops);
+
+            // Finish running the game
+            for (UInt64 i = placed_after_loops; i < total_piece_count; i++)
+            {
+                board.move_until_settled(pretty);
+
+                if (pretty)
+                {
+                    board.display();
+                }
+            }
+
+            if (!pretty)
+            {
+                board.display();
+            }
+
+            // And display the total tower height.
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.WriteLine("Result = {0}", board.get_highest_nonempty_spot());
+            Console.ResetColor();
         }
     }
 }
