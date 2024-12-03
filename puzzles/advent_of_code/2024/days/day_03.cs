e7e4ea343f239b39814fa81a23a23c65a73a8704
LEPT0N <dan.mitchell@outlook.com>
2024-12-02 21:34:50 -0800

    Advent of Code 2024 Day 03

---
+++ puzzles/advent_of_code/2024/days/day_03.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using advent_of_code_common.input_reader;
+
+namespace advent_of_code_2024.days
+{
+	internal class day_03
+	{
+		public static void part_1(
+			string input,
+			bool pretty)
+		{
+			c_input_reader input_reader = new c_input_reader(input);
+
+			Regex mul_format = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+
+			int result = 0;
+
+			while (input_reader.has_more_lines())
+			{
+				string input_line = input_reader.read_line();
+
+				MatchCollection mul_matches = mul_format.Matches(input_line);
+
+				foreach (Match mul_match in mul_matches)
+				{
+					int first = int.Parse(mul_match.Groups[1].Value);
+					int second = int.Parse(mul_match.Groups[2].Value);
+
+					result += first * second;
+				}
+			}
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine();
+			Console.WriteLine("Result = {0}", result);
+			Console.ResetColor();
+		}
+
+		public static void part_2(
+			string input,
+			bool pretty)
+		{
+			c_input_reader input_reader = new c_input_reader(input);
+
+			Regex mul_format = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))");
+
+			int result = 0;
+			bool enabled = true;
+
+			while (input_reader.has_more_lines())
+			{
+				string input_line = input_reader.read_line();
+
+				MatchCollection mul_matches = mul_format.Matches(input_line);
+
+				foreach (Match mul_match in mul_matches)
+				{
+					if (!string.IsNullOrEmpty(mul_match.Groups[1].Value))
+					{
+						if (enabled)
+						{
+							// Found a mul
+
+							int first = int.Parse(mul_match.Groups[1].Value);
+							int second = int.Parse(mul_match.Groups[2].Value);
+
+							result += first * second;
+						}
+					}
+					else if (!string.IsNullOrEmpty(mul_match.Groups[3].Value))
+					{
+						// Found a do
+						enabled = true;
+					}
+					else if (!string.IsNullOrEmpty(mul_match.Groups[4].Value))
+					{
+						// Found a don't
+						enabled = false;
+					}
+					else
+					{
+						throw new Exception("oops");
+					}
+				}
+			}
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine();
+			Console.WriteLine("Result = {0}", result);
+			Console.ResetColor();
+		}
+	}
+}
+
+// not right: 28546082
