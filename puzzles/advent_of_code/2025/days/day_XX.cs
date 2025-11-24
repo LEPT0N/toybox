5a68ccf57a2e30028d963b3c0b58bb18bd0c4db0
LEPT0N <27709682+LEPT0N@users.noreply.github.com>
2025-11-24 14:52:18 -0800

    Advent of Code 2025 Prep

---
+++ puzzles/advent_of_code/2025/days/day_XX.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using advent_of_code_common.input_reader;
+
+namespace advent_of_code_2025.days
+{
+    internal class day_XX
+    {
+        [DebuggerDisplay("todo", Type = "c_type")]
+        internal class c_type
+        {
+#pragma warning disable 0169
+            int unused;
+#pragma warning restore 0169
+        }
+
+        internal static void parse_input(
+            in c_input_reader input_reader,
+            in bool pretty)
+        {
+            while (input_reader.has_more_lines())
+            {
+                input_reader.read_line();
+            }
+        }
+
+        public static void part_1(
+            c_input_reader input_reader,
+            bool pretty)
+        {
+            parse_input(input_reader, pretty);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.WriteLine($"Result = {0}");
+            Console.ResetColor();
+        }
+
+        public static void part_2(
+            c_input_reader input_reader,
+            bool pretty)
+        {
+            // parse_input(input_reader, pretty);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.WriteLine($"Result = {0}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ puzzles/advent_of_code/2025/main.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using advent_of_code_common.extensions;
+using advent_of_code_common.input_reader;
+
+namespace advent_of_code_2025
+{
+    internal class main
+    {
+        public static bool pretty { get; private set; }
+        public static HashSet<string> options { get; private set; }
+
+        static void Main(string[] args)
+        {
+            List<string> arguments = args.ToList();
+
+            int day = Int32.Parse(arguments.remove_first());
+            int part = Int32.Parse(arguments.remove_first());
+
+            string input_file = arguments.remove_first();
+            input_file = input_file.Replace("{day}", $"{day:00}");
+
+            options = new HashSet<string>(arguments);
+
+            pretty = options.Contains("pretty");
+
+            string class_name = $"advent_of_code_2025.days.day_{day:00}";
+            Type class_type = Type.GetType(class_name);
+
+            string method_name = $"part_{part}";
+            MethodInfo method = class_type?.GetMethod(method_name);
+
+            if (method == null)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Unable to find {class_name}.{method_name}");
+                Console.ResetColor();
+
+                return;
+            }
+
+            c_input_reader input = new c_input_reader(input_file);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            method.Invoke(null, new object[] { input, pretty });
+
+            stopwatch.Stop();
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Time taken = {stopwatch.Elapsed}");
+            Console.ResetColor();
+        }
+    }
+}
